0fb7dbeae9ef3ba8acae534712cc1e12e9254b9b
b1ek <me@blek.codes>
2025-07-30 22:16:36 +1000

    add survey API

---
+++ app/Http/Requests/SurveyEntryRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SurveyEntryRequest extends APIKeyRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if (!parent::authorize()) {
+            return false;
+        }
+
+        return $this->key->hasScope('survey-entry');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'text' => 'required|string',
+        ];
+    }
+}
